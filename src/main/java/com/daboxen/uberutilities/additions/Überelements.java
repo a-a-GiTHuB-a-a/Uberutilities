dc36bf7826cdf662a2c8f1385298a1005b39cbf6
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-11-19 18:52:49 -0500

    separate materials and elements

---
+++ src/main/java/com/daboxen/uberutilities/additions/Überelements.java
@@ -0,0 +1,15 @@
+package com.daboxen.uberutilities.additions;
+
+import com.gregtechceu.gtceu.api.data.chemical.Element;
+import com.gregtechceu.gtceu.api.registry.GTRegistries;
+
+public class Überelements {
+	//thanks for the code! yoinked from cosmic core
+	public static Element createAndRegisterElement(long protons, long neutrons, long halfLifeSeconds, String decayTo, String name, String symbol, boolean isIsotope) {
+		Element element = new Element(protons, neutrons, halfLifeSeconds, decayTo, name, symbol, isIsotope);
+		GTRegistries.ELEMENTS.register(name, element);
+		return element;
+	}
+
+	public static final Element AntiNeutronium = createAndRegisterElement(0, 1000, -1, null, "anti_neutronium", "!Nt", false);
+}
--- src/main/java/com/daboxen/uberutilities/additions/Übermaterials.java
+++ src/main/java/com/daboxen/uberutilities/additions/Übermaterials.java
@@ -2,53 +2,48 @@ package com.daboxen.uberutilities.additions;
 
 import com.daboxen.uberutilities.Überutilities;
 import com.gregtechceu.gtceu.api.GTValues;
-import com.gregtechceu.gtceu.api.data.chemical.Element;
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
 import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialFlags;
 import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialIconSet;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey;
 import com.gregtechceu.gtceu.api.data.chemical.material.properties.WireProperties;
-import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 
 public class Übermaterials {
-	public static void init() {}
+	public static Material UpQuark;
+	public static Material AntiUpQuark;
+	public static Material DownQuark;
+	public static Material AntiDownQuark;
+
+	public static Material AntiNeutronium;
+	public static void init() {
+		UpQuark = new Material.Builder(Überutilities.id("up_quark"))
+			.gas(0)
+			.color(0xff0000)
+			.buildAndRegister();
+		AntiUpQuark = new Material.Builder(Überutilities.id("anti_up_quark"))
+			.gas(0)
+			.color(0xff0000)
+			.buildAndRegister();
+		DownQuark = new Material.Builder(Überutilities.id("down_quark"))
+			.gas(0)
+			.color(0xff0000)
+			.buildAndRegister();
+		AntiDownQuark = new Material.Builder(Überutilities.id("anti_down_quark"))
+			.gas(0)
+			.color(0xff0000)
+			.buildAndRegister();
+		AntiNeutronium = new Material.Builder(Überutilities.id("anti_neutronium"))
+			.element(Überelements.AntiNeutronium)
+			.color(0x000000)
+			.secondaryColor(0xffffff)
+			.iconSet(MaterialIconSet.RADIOACTIVE)
+			.ingot().fluid()
+			.buildAndRegister();
+	}
 
 	public static void modifyMaterials() {
 		GTMaterials.Duranium.setProperty(PropertyKey.WIRE, new WireProperties((int)GTValues.V[GTValues.UHV], 4, 96));
         GTMaterials.Neutronium.addFlags(MaterialFlags.GENERATE_LONG_ROD, MaterialFlags.GENERATE_ROUND);
 	}
-
-	//thanks for the code! yoinked from cosmic core
-	public static Element createAndRegisterElement(long protons, long neutrons, long halfLifeSeconds, String decayTo, String name, String symbol, boolean isIsotope) {
-		Element element = new Element(protons, neutrons, halfLifeSeconds, decayTo, name, symbol, isIsotope);
-		GTRegistries.ELEMENTS.register(name, element);
-		return element;
-	}
-
-	public static final Element ElementAntiNeutronium = createAndRegisterElement(0, 1000, -1, null, "anti_neutronium", "!Nt", false);
-
-	public static final Material UpQuark = new Material.Builder(Überutilities.id("up_quark"))
-		.gas(0)
-		.color(0xff0000)
-		.buildAndRegister();
-	public static final Material AntiUpQuark = new Material.Builder(Überutilities.id("anti_up_quark"))
-		.gas(0)
-		.color(0xff0000)
-		.buildAndRegister();
-	public static final Material DownQuark = new Material.Builder(Überutilities.id("down_quark"))
-		.gas(0)
-		.color(0xff0000)
-		.buildAndRegister();
-	public static final Material AntiDownQuark = new Material.Builder(Überutilities.id("anti_down_quark"))
-		.gas(0)
-		.color(0xff0000)
-		.buildAndRegister();
-	public static final Material AntiNeutronium = new Material.Builder(Überutilities.id("anti_neutronium"))
-		.element(ElementAntiNeutronium)
-		.color(0x000000)
-		.secondaryColor(0xffffff)
-		.iconSet(MaterialIconSet.RADIOACTIVE)
-		.ingot().fluid()
-		.buildAndRegister();
 }
