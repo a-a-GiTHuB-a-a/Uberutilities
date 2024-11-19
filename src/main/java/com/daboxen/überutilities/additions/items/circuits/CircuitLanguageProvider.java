e02591d3f3959026956055cb6219095d8a3e0d0f
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-11-18 21:51:06 -0500

    finish the all-tiers migration

--- src/main/java/com/daboxen/überutilities/additions/items/circuits/CircuitLanguageProvider.java
+++ src/main/java/com/daboxen/überutilities/additions/items/circuits/CircuitLanguageProvider.java
@@ -1,5 +1,6 @@
 package com.daboxen.überutilities.additions.items.circuits;
 
+import com.gregtechceu.gtceu.api.GTValues;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 
@@ -19,6 +20,33 @@ public class CircuitLanguageProvider extends LanguageProvider {
 		this.add(
 			String.format("item.überutilities.%s_processor.tooltip.1", type.name.toLowerCase()),
 			String.format("§%c%s-Tier Circuit§r", type.color, GTValues.VN[type.tier])
+		);
+
+		this.add(
+			String.format("item.überutilities.%s_processor_assembly.tooltip.0", type.name.toLowerCase()),
+			type.descriptions[1]
+		);
+		this.add(
+			String.format("item.überutilities.%s_processor_assembly.tooltip.1", type.name.toLowerCase()),
+			String.format("§%c%s-Tier Circuit§r", type.color, GTValues.VN[type.tier + 1])
+		);
+
+		this.add(
+			String.format("item.überutilities.%s_processor_computer.tooltip.0", type.name.toLowerCase()),
+			type.descriptions[2]
+		);
+		this.add(
+			String.format("item.überutilities.%s_processor_computer.tooltip.1", type.name.toLowerCase()),
+			String.format("§%c%s-Tier Circuit§r", type.color, GTValues.VN[type.tier + 2])
+		);
+
+		this.add(
+			String.format("item.überutilities.%s_processor_mainframe.tooltip.0", type.name.toLowerCase()),
+			type.descriptions[3]
+		);
+		this.add(
+			String.format("item.überutilities.%s_processor_mainframe.tooltip.1", type.name.toLowerCase()),
+			String.format("§%c%s-Tier Circuit§r", type.color, GTValues.VN[type.tier + 3])
 		);
 	}
 }
