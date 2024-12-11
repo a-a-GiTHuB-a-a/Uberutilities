83f7ee7a3bc02a5c5f7b7eee764282c4f1b166d4
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-11 17:12:20 -0500

    add logger to `IconSets`

--- src/main/java/com/daboxen/uberutilities/api/IconSets.java
+++ src/main/java/com/daboxen/uberutilities/api/IconSets.java
@@ -1,5 +1,6 @@
 package com.daboxen.uberutilities.api;
 
+import static com.daboxen.uberutilities.Uberutilities.LOGGER;
 import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialIconSet;
 
 public class IconSets {
@@ -7,6 +8,6 @@ public class IconSets {
 	public static final MaterialIconSet SUS = new MaterialIconSet("sus", MaterialIconSet.METALLIC);
 
 	public static void init() {
-
+		LOGGER.info("Adding material icons");
 	}
 }
