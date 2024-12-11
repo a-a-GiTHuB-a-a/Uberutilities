a36069b1a87e4dccea14f1767cae9d148b5c384d
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-11 18:25:09 -0500

    add info

--- src/main/java/com/daboxen/uberutilities/api/RecipeTypes.java
+++ src/main/java/com/daboxen/uberutilities/api/RecipeTypes.java
@@ -2,18 +2,19 @@ package com.daboxen.uberutilities.api;
 
 import static com.daboxen.uberutilities.Uberutilities.LOGGER;
 
-import com.gregtechceu.gtceu.api.capability.recipe.FluidRecipeCapability;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
-import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
 import com.gregtechceu.gtceu.api.gui.GuiTextures;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
+import com.gregtechceu.gtceu.common.data.GTSoundEntries;
 import com.lowdragmc.lowdraglib.gui.texture.ProgressTexture;
 
 public class RecipeTypes {
 	public static final GTRecipeType FIELD_EXCITER = GTRecipeTypes.register("field_exciter", GTRecipeTypes.MULTIBLOCK)
 		.setMaxIOSize(1, 0, 0, 2)
-		.setProgressBar(GuiTextures.PROGRESS_BAR_FUSION, ProgressTexture.FillDirection.LEFT_TO_RIGHT);
+		.setProgressBar(GuiTextures.PROGRESS_BAR_FUSION, ProgressTexture.FillDirection.LEFT_TO_RIGHT)
+		.setSound(GTSoundEntries.ELECTROLYZER)
+		.setEUIO(IO.IN);
 	
 	public static void init() {
 		LOGGER.info("Registering recipe types");
