f887c4ae9f6b24ba9e2c38440e25531e6cd51710
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-14 14:09:00 -0500

    yoink code lol

--- src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
+++ src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
@@ -1,14 +1,37 @@
 package com.daboxen.uberutilities.additions.items;
 
 import com.daboxen.uberutilities.Uberutilities;
-import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.api.item.tool.GTToolType;
+import com.gregtechceu.gtceu.common.data.GTModels;
+import com.tterrag.registrate.builders.BlockBuilder;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import static com.daboxen.uberutilities.Uberutilities.LOGGER;
+import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 
 public class Uberblocks {
-	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = GTBlocks.createCasingBlock("neutronium_casing", Uberutilities.id("blocks/neutronium_casing"));
+	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = createCasing("neutronium_casing", Uberutilities.id("blocks/neutronium_casing"));
+	
+	public static BlockEntry<Block> createCasing(String name, ResourceLocation texture) {
+		return createCasing(name, texture, 2);
+	}
+	
+	public static BlockEntry<Block> createCasing(String name, ResourceLocation texture, int tier) {
+		return UBER_REGISTRATE.block(name, Block::new)
+			.initialProperties(() -> Blocks.IRON_BLOCK)
+			.properties(p -> p.isValidSpawn((state, level, pos, ent) -> false))
+			.properties(p -> (tier >= 3 ? p.explosionResistance(-1) : p))
+			.blockstate(GTModels.cubeAllModel(name, texture))
+			.tag(GTToolType.WRENCH.harvestTags.get(0), BlockTags.MINEABLE_WITH_PICKAXE)
+			.item(BlockItem::new)
+			.build()
+			.register();
+	}
 	
 	public static void init() {
 		LOGGER.info("Registering custom blocks");
