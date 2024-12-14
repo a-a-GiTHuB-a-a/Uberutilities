4dde1917faee466a4171646965903aea86eb3a8f
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-14 15:17:53 -0500

    heruihtuior

--- src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
+++ src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
@@ -3,7 +3,6 @@ package com.daboxen.uberutilities.additions.items;
 import com.daboxen.uberutilities.Uberutilities;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.common.data.GTModels;
-import com.tterrag.registrate.builders.BlockBuilder;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
@@ -11,22 +10,25 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import javax.annotation.Nonnull;
+
 import static com.daboxen.uberutilities.Uberutilities.LOGGER;
 import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 
 public class Uberblocks {
-	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = createCasing("neutronium_casing");
+	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = createCasing("neutronium_casing", Uberutilities.id("block/neutronium_casing"));
 	
-	public static BlockEntry<Block> createCasing(String name) {
-		return createCasing(name, 2);
+	public static @Nonnull BlockEntry<Block> createCasing(@Nonnull String name, @Nonnull ResourceLocation texture) {
+		return createCasing(name, texture, 2);
 	}
 	
-	public static BlockEntry<Block> createCasing(String name, int tier) {
+	public static @Nonnull BlockEntry<Block> createCasing(@Nonnull String name, @Nonnull ResourceLocation texture, int tier) {
 		return UBER_REGISTRATE.block(name, Block::new)
 			.initialProperties(() -> Blocks.IRON_BLOCK)
 			.properties(p -> p.isValidSpawn((state, level, pos, ent) -> false))
 			.properties(p -> (tier >= 3 ? p.explosionResistance(-1) : p))
 			.tag(GTToolType.WRENCH.harvestTags.get(0), BlockTags.MINEABLE_WITH_PICKAXE)
+			.blockstate(GTModels.cubeAllModel(name, texture))
 			.item(BlockItem::new)
 			.build()
 			.register();
