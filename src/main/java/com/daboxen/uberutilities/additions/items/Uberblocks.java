7899027e3756a83d5e1e19096ca095a1f0b4823b
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-14 13:37:23 -0500

    leech off GT's casing method

--- src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
+++ src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
@@ -1,26 +1,14 @@
 package com.daboxen.uberutilities.additions.items;
 
 import com.daboxen.uberutilities.Uberutilities;
+import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 
 import static com.daboxen.uberutilities.Uberutilities.LOGGER;
-import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 
 public class Uberblocks {
-	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = generateCasing("neutronium_casing", Uberutilities.id("block/neutronium_casing"));
-	
-	public static BlockEntry<Block> generateCasing(String name, ResourceLocation texture) {
-		return UBER_REGISTRATE.block(name, Block::new)
-			.tag(TagKey.create(Registries.BLOCK, new ResourceLocation("forge", "mineable/wrench")))
-			.item(BlockItem::new)
-			.build()
-			.register();
-	}
+	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = GTBlocks.createCasingBlock("neutronium_casing", Uberutilities.id("blocks/neutronium_casing"));
 	
 	public static void init() {
 		LOGGER.info("Registering custom blocks");
