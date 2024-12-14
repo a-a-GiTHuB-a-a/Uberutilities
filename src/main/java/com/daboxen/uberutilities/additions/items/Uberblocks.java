8619e60833e20a67fe2a407e88870b4101e5f06b
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-12-14 14:48:21 -0500

    yeah we're oversimplifying

--- src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
+++ src/main/java/com/daboxen/uberutilities/additions/items/Uberblocks.java
@@ -15,18 +15,17 @@ import static com.daboxen.uberutilities.Uberutilities.LOGGER;
 import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 
 public class Uberblocks {
-	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = createCasing("neutronium_casing", Uberutilities.id("block/neutronium_casing"));
+	public static BlockEntry<Block> NEUTRONIUM_MACHINE_CASING = createCasing("neutronium_casing");
 	
-	public static BlockEntry<Block> createCasing(String name, ResourceLocation texture) {
-		return createCasing(name, texture, 2);
+	public static BlockEntry<Block> createCasing(String name) {
+		return createCasing(name, 2);
 	}
 	
-	public static BlockEntry<Block> createCasing(String name, ResourceLocation texture, int tier) {
+	public static BlockEntry<Block> createCasing(String name, int tier) {
 		return UBER_REGISTRATE.block(name, Block::new)
 			.initialProperties(() -> Blocks.IRON_BLOCK)
 			.properties(p -> p.isValidSpawn((state, level, pos, ent) -> false))
 			.properties(p -> (tier >= 3 ? p.explosionResistance(-1) : p))
-			.blockstate(GTModels.cubeAllModel(name, texture))
 			.tag(GTToolType.WRENCH.harvestTags.get(0), BlockTags.MINEABLE_WITH_PICKAXE)
 			.item(BlockItem::new)
 			.build()
