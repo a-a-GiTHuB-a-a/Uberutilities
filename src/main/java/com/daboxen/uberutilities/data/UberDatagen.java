45667ab96a17cb9e1158addd52b7d3d6d70239cf
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-01-08 21:07:28 -0500

    add langgen!!!

---
+++ src/main/java/com/daboxen/uberutilities/data/UberDatagen.java
@@ -0,0 +1,11 @@
+package com.daboxen.uberutilities.data;
+
+import com.daboxen.uberutilities.data.lang.LangHandler;
+import com.tterrag.registrate.providers.ProviderType;
+import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
+
+public class UberDatagen {
+	public static void init() {
+		UBER_REGISTRATE.addDataGenerator(ProviderType.LANG, LangHandler::init);
+	}
+}
