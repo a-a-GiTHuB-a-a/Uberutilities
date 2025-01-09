313a7f039afec74c64ce571fcbe3fe4f195dbce0
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-01-09 17:23:35 -0500

    remove dupes

--- src/main/java/com/daboxen/uberutilities/data/lang/LangHandler.java
+++ src/main/java/com/daboxen/uberutilities/data/lang/LangHandler.java
@@ -7,15 +7,10 @@ public class LangHandler {
 		//todo: outsource langgen to the actual places where all this stuff is made
 		
 		provider.add("material.uberutilities.anti_neutronium", "Anti-Neutronium");
-		provider.add("material.uberutilities.up_quark", "Up Quark");
-		provider.add("material.uberutilities.anti_up_quark", "Anti Up Quark");
-		provider.add("material.uberutilities.down_quark", "Down Quark");
-		provider.add("material.uberutilities.anti_down_quark", "Anti Down Quark");
 		
 		provider.add("uberutilities.fluid.type_antimatter.tooltip", "§khello§r§c§lANTIMATTER! HANDLE WITH EXTREME CAUTION!§r§kworld§r");
 		provider.add("uberutilities.fluid_pipe.antimatter_proof", "§khello§r§c§lCapable of containing antimatter§r§kworld§r");
 		
 		provider.add("machine.uberutilities.field_exciter.tooltip.0", "Making matter out of pure energy");
-		provider.add("block.uberutilities.field_exciter", "Quantum Field Exciter");
 	}
 }
