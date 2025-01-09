012736b309bc85de040838384a765ede3404a10c
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-01-09 17:28:39 -0500

    some movings-around

--- src/main/java/com/daboxen/uberutilities/data/lang/LangHandler.java
+++ src/main/java/com/daboxen/uberutilities/data/lang/LangHandler.java
@@ -1,16 +1,18 @@
 package com.daboxen.uberutilities.data.lang;
 
+import static com.gregtechceu.gtceu.data.lang.LangHandler.replace;
 import com.tterrag.registrate.providers.RegistrateLangProvider;
 
 public class LangHandler {
 	public static void init(RegistrateLangProvider provider) {
 		//todo: outsource langgen to the actual places where all this stuff is made
 		
-		provider.add("material.uberutilities.anti_neutronium", "Anti-Neutronium");
+		replace(provider, "material.uberutilities.anti_neutronium", "Anti-Neutronium");
 		
 		provider.add("uberutilities.fluid.type_antimatter.tooltip", "§khello§r§c§lANTIMATTER! HANDLE WITH EXTREME CAUTION!§r§kworld§r");
 		provider.add("uberutilities.fluid_pipe.antimatter_proof", "§khello§r§c§lCapable of containing antimatter§r§kworld§r");
 		
 		provider.add("machine.uberutilities.field_exciter.tooltip.0", "Making matter out of pure energy");
+		replace(provider, "block.uberutilities.field_exciter", "Quantum Field Exciter");
 	}
 }
