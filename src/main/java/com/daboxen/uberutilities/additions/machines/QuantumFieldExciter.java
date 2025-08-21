89eed5bf4bc9e5212a922622606f902d756d7e58
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-08-20 21:56:43 -0400

    THE MULTIBLOCK AGE

--- src/main/java/com/daboxen/uberutilities/additions/machines/QuantumFieldExciter.java
+++ src/main/java/com/daboxen/uberutilities/additions/machines/QuantumFieldExciter.java
@@ -1,20 +1,24 @@
 package com.daboxen.uberutilities.additions.machines;
 
 import com.daboxen.uberutilities.Uberutilities;
-import static com.daboxen.uberutilities.Uberutilities.LOGGER;
-import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 import com.daboxen.uberutilities.additions.items.Uberblocks;
 import com.daboxen.uberutilities.api.RecipeTypes;
-import com.daboxen.uberutilities.data.lang.LangHandler;
+import static com.daboxen.uberutilities.Uberutilities.LOGGER;
+import static com.daboxen.uberutilities.Uberutilities.UBER_REGISTRATE;
 import com.gregtechceu.gtceu.GTCEu;
+import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 import com.gregtechceu.gtceu.api.data.RotationState;
+import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMachine;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
+import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.DyeColor;
 
 public class QuantumFieldExciter {
 	public static final MultiblockMachineDefinition FIELD_EXCITER = UBER_REGISTRATE.multiblock("field_exciter", WorkableElectricMultiblockMachine::new)
@@ -24,24 +28,38 @@ public class QuantumFieldExciter {
 		.appearanceBlock(Uberblocks.NEUTRONIUM_MACHINE_CASING)
 		//spotless:off
 		.pattern(definition -> FactoryBlockPattern.start()
-			.aisle("           ", "           ", "           ", "   NNNNN   ", "   NNNNN   ", "   NNNNN   ", "   NNNNN   ", "   NNNNN   ", "           ", "           ", "           ")
-			.aisle("           ", " NNNNNNNNN ", " NNNNNNNNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNNNNNNNN ", " NNNNNNNNN ", "           ")
-			.aisle("           ", " NNNNNNNNN ", " NQQQQQQQN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQQQQQQQN ", " NNNNNNNNN ", "           ")
-			.aisle("   NNNNN   ", " NNQQQQQNN ", " NQ-----QN ", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", " NQ-----QN ", " NNQQQQQNN ", "   NNNNN   ")
-			.aisle("   NNNNN   ", " NNQQQQQNN ", " NQ-----QN ", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", " NQ-----QN ", " NNQQQQQNN ", "   NNNNN   ")
-			.aisle("   NNNNN   ", " NNQQQQQNN ", " NQ-----QN ", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", " NQ-----QN ", " NNQQQQQNN ", "   NNNNN   ")
-			.aisle("   NNNNN   ", " NNQQQQQNN ", " NQ-----QN ", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", " NQ-----QN ", " NNQQQQQNN ", "   NNNNN   ")
-			.aisle("   NNNNN   ", " NNQQQQQNN ", " NQ-----QN ", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", "NQ-------QN", " NQ-----QN ", " NNQQQQQNN ", "   NNNNN   ")
-			.aisle("           ", " NNNNNNNNN ", " NQQQQQQQN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQ-----QN ", " NQQQQQQQN ", " NNNNNNNNN ", "           ")
-			.aisle("           ", " NNNNNNNNN ", " NNNNNNNNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNQQQQQNN ", " NNNNNNNNN ", " NNNNNNNNN ", "           ")
-			.aisle("           ", "           ", "           ", "   NNNNN   ", "   NNNNN   ", "   NNNNN   ", "   NNMNN   ", "   NNNNN   ", "           ", "           ", "           ")
-			.where('M', Predicates.controller(Predicates.blocks(definition.get())))
-			.where('N', Predicates.blocks(Uberblocks.NEUTRONIUM_MACHINE_CASING.get())
-				.or(Predicates.autoAbilities(RecipeTypes.FIELD_EXCITER))
-			)
-			.where('Q', Predicates.blocks(Uberblocks.NAQUADAH_MACHINE_CASING.get()))
-			.where(' ', Predicates.any())
-			.where('-', Predicates.air())
+			.aisle("                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "        NNNNN        ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NNQQ--D--QQNN    ", "    NNQQ-DKD-QQNN    ", "    NNQQ--D--QQNN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQQ----D----QQN   ", "   NQQ---DKD---QQN   ", "   NQQ----D----QQN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "     NQQ-----QQN     ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "  NQ-------------QN  ", "  NQ------D------QN  ", "  NQ-----DKD-----QN  ", "  NQ------D------QN  ", "  NQ-------------QN  ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "     NQQ-----QQN     ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NQ---------QN    ", "   NQ-----------QN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ------D------QN  ", "  NQ-----DKD-----QN  ", "  NQ------D------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQ-----------QN   ", "    NQ---------QN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ")
+			.aisle("                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ-------D-------QN ", " NQ------DKD------QN ", " NQ-------D-------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ")
+			.aisle("                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ---------------QN ", " NQ-------K-------QN ", " NQ---------------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ")
+			.aisle("        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ---------------QN ", "NQ-----------------QN", "NQ-----------------QN", "NQ--------K--------QN", "NQ-----------------QN", "NQ-----------------QN", " NQ---------------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ")
+			.aisle("        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ--D--QQNN    ", "   NQQ----D----QQN   ", "  NQ------D------QN  ", "  NQ------D------QN  ", " NQ-------D-------QN ", " NQ---------------QN ", "NQ-----------------QN", "NQ-----------------QN", "NQDDDDD---H---DDDDDQN", "NQ-----------------QN", "NQ-----------------QN", " NQ---------------QN ", " NQ-------D-------QN ", "  NQ------D------QN  ", "  NQ------D------QN  ", "   NQQ----D----QQN   ", "    NNQQ--D--QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ")
+			.aisle("        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-DED-QQNN    ", "   NQQ---DED---QQN   ", "  NQ-----DED-----QN  ", "  NQ-----DED-----QN  ", " NQ------DED------QN ", " NQ-------I-------QN ", "NQ--------I--------QN", "NQDDDDD---H---DDDDDQN", "NQEEEEEGGH-HGGEEEEEQN", "NQDDDDD---H---DDDDDQN", "NQ--------I--------QN", " NQ-------I-------QN ", " NQ------DED------QN ", "  NQ-----DED-----QN  ", "  NQ-----DED-----QN  ", "   NQQ---DED---QQN   ", "    NNQQ-DED-QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ")
+			.aisle("        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ--D--QQNN    ", "   NQQ----D----QQN   ", "  NQ------D------QN  ", "  NQ------D------QN  ", " NQ-------D-------QN ", " NQ---------------QN ", "NQ-----------------QN", "NQ-----------------QN", "NQDDDDD-------DDDDDQN", "NQ-----------------QN", "NQ-----------------QN", " NQ---------------QN ", " NQ-------D-------QN ", "  NQ------D------QN  ", "  NQ------D------QN  ", "   NQQ----D----QQN   ", "    NNQQ--D--QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ")
+			.aisle("        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ---------------QN ", "NQ-----------------QN", "NQ-----------------QN", "NQ-----------------QN", "NQ-----------------QN", "NQ-----------------QN", " NQ---------------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ")
+			.aisle("                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ")
+			.aisle("                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", " NQ---------------QN ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ")
+			.aisle("                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NQ---------QN    ", "   NQ-----------QN   ", "   NQ-----------QN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQ-----------QN   ", "   NQ-----------QN   ", "    NQ---------QN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "     NQQ-----QQN     ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "  NQ-------------QN  ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "     NQQ-----QQN     ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "   NQQ---------QQN   ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "    NNQQ-----QQNN    ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "      NNQQQQQNN      ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ")
+			.aisle("                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "        NNNNN        ", "        NNNNN        ", "        NNMNN        ", "        NNNNN        ", "        NNNNN        ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ", "                     ")
+			.where(" ", Predicates.any())
+			.where("-", Predicates.air())
+			.where("M", Predicates.controller(Predicates.blocks(definition.get())))
+			.where("N", Predicates.blocks(Uberblocks.NEUTRONIUM_MACHINE_CASING.get()))
+			.where("Q", Predicates.blocks(Uberblocks.NAQUADAH_MACHINE_CASING.get()))
+			.where("D", Predicates.blocks(ChemicalHelper.getBlock(TagPrefix.frameGt, NaquadahAlloy)))
+			.where("E", Predicates.blocks(GTBlocks.BORDERLESS_LAMPS.get(DyeColor.RED)))
+			.where("G", Predicates.blocks(ChemicalHelper.getBlock(TagPrefix.block, Naquadah)))
+			.where("H", Predicates.blocks(GTBlocks.MACHINE_CASING_UEV.get()))
+			.where("I", Predicates.blocks(ChemicalHelper.getBlock(TagPrefix.block, Neutronium)))
+			.where("K", Predicates.blocks(GTBlocks.HIGH_POWER_CASING.get()))
 			.build()
 		)
 		//spotless:on
